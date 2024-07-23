6c093ac90c02cff343477fe698b92be2b2cd593c
JakubSchwenkbeck <jakub@schwenkbeck.de>
2024-07-23 20:48:10 +0200

    changed the way the data structure is handled

--- Main.m
+++ Main.m
@@ -7,7 +7,7 @@ function Main
 % data = loadAllData("C:\Users\jakub\OneDrive\Dokumente\MATLAB\AV-Classifier\SensorData\4 Actors");
 % 
 
-labels = loadAllLabels(2,size);
+labels = createLabels(2,size);
     
 net = createRNN(data,labels);
 disp(net);
@@ -32,49 +32,21 @@ for i = 1:length(matFiles)
     % Load the .mat file
     dataStruct = load(filePath);
     
-    % Store the data in the cell array
+    % Store the data in the cell arrayclc
     dataCellArray{i} = dataStruct;
 
-        disp(dataCellArray{i});
-    allData{i} = preprocessSensorData( dataCellArray{i}.(filenameWithoutExtension));
+        %disp(dataCellArray{i});
+         fields = fieldnames(dataCellArray{i});
+    
+    % Assuming there is only one field per struct, get the first field name
+    filenameField = fields{1};
+    allData{i} = preprocessSensorData( dataCellArray{i}.(filenameField));
     
  end
 
 
 end
-function allLabels = loadAllLabels(numActor,size)
-
-labelsCellArray = cell(size, 1);
-
-if(numActor ==2 )
-%AEB 2
-label1  =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.1,0.1,0.1,0.1,0.12,0.13,0.14,0.15,0.2,0.2,0.2,0.22,0.23,0.24,0.25,0.26,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,6,0.6,0.6,0.6,0.6,0.75,0.75,0.75,0.75,0.75,0.86,0.86,0.86,0.86,0.86,0.8,0.8,0.7];
-%AEB 3
-label2  =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.05,0.1,0.1,0.1,0.11,0.12,0.12,0.13,0.14,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.16,0.17,0.2,0.205,0.21,0.2,0.22,0.23,0.24,0.25,0.26,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.3,0.3,0.3,0.3,0.25,0.25,0.22,0.2 ];
-
-label3 = [0,0,0,0,0,0,0,05,05,0.1,0.1,0.1,0.1,0.15,0.15,0.15,0.2,0.2,0.25,0.3,0.3,0.35,0.4,0.45,0.55,0.5,0.5,0.6,0.6,0.7,0.75,0.8,0.85,0.95,0.9,0.9,0.9,0.9,0.9,0.9,1];
-allLabels{1} = categorical(label1);
-allLabels{2} = categorical(label2);
-allLabels{3} = categorical(label3);
-
-elseif(numActor == 3)
 
 
-elseif(numActor == 4)
 
-
-end
-% AEB 1
-% label2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.05,0.1,0.15,0.2,0.4,0.5,0.55,0.6,0.7,0.8,0.9,0.9,1,1];
-% % AEB 2
-% label1  =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.1,0.1,0.1,0.1,0.12,0.13,0.14,0.15,0.2,0.2,0.2,0.2,0.2,0.2,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,6,0.6,0.6,0.6,0.6,0.75,0.75,0.75,0.75,0.75,0.86,0.86,0.86,0.86,0.86,0.8,0.8,0.7]
-% %AEB 3
-% 
-% label3  =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.1,0.1,0.1,0.1,0.12,0.12,0.12,0.12,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.2,0.2,0.2,0.2,0.22,0.23,0.24,0.25,0.26,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.3,0.3,0.3,0.3,0.25,0.25,0.22,0.2 ]
-% 
-% 
-% allLabels{1} = categorical(label1);
-% allLabels{2} = categorical(label2);
-
-end
 

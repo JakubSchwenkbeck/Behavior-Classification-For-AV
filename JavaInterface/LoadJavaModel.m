090fa3d87523d8e29cfb70250403f1bc624c192c
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2024-09-02 00:56:45 +0200

    added Outlines for JavaInterface

---
+++ JavaInterface/LoadJavaModel.m
@@ -0,0 +1,29 @@
+function javaInterface()
+    % Add the Java JAR file to the MATLAB Java class path
+    javaaddpath('path/to/the/JavaNeuralNetwork.jar');
+
+    % Create an instance of the Java class
+    javaObj = javaObject('mypackage.JavaNeuralNetwork');
+
+    % Load the neural network model
+    modelPath = 'path/to/my/model';
+    javaObj.loadNetwork(modelPath);
+
+    % Prepare input data
+    inputData = [1.0, 2.0, 3.0]; % Example input data
+    inputDataJava = javaArray('java.lang.Double', length(inputData));
+    for i = 1:length(inputData)
+        inputDataJava(i) = java.lang.Double(inputData(i));
+    end
+
+    % Perform a prediction
+    outputDataJava = javaObj.predict(inputDataJava);
+    outputData = zeros(1, length(outputDataJava));
+    for i = 1:length(outputDataJava)
+        outputData(i) = outputDataJava(i).doubleValue();
+    end
+
+    % Display the result
+    disp('Prediction results:');
+    disp(outputData);
+end

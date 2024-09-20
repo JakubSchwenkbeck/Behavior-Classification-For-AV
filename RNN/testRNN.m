4b98baeb9dc55426e4f57c70dd63d4cb4245093a
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2024-09-20 15:21:45 +0200

    Update testRNN.m

--- RNN/testRNN.m
+++ RNN/testRNN.m
@@ -9,7 +9,7 @@ function predictedLabels = testRNN(TrainedNet, filename)
     %   predictedLabels (categorical): The labels predicted by the RNN for the input data.
     
     % Define the folder path where the .mat file is located
-    folderPath = "C:\Users\jakub\OneDrive\Dokumente\MATLAB\AV-Classifier";
+    folderPath = "...\MATLAB\AV-Classifier"; % ADJUST TO YOUR PATH
     
     % Remove the .mat extension from the filename to get the variable name
     filenameWithoutExtension = strrep(filename, '.mat', '');
